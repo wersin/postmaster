2f857e084e783b07b580e571618b4009ef9c1ccf
mark <mark@bley.com>
2018-06-22 11:19:27 +0200

    first commit

---
+++ MotorPlusSerial.ino
@@ -0,0 +1,105 @@
+char inByte;
+String command;
+String Mode;
+String Speed1;
+String Speed2;
+char Speed1Array[4];
+char Speed2Array[4];
+int Speed1Int;
+int Speed2Int;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(3,OUTPUT);       //Channel A Speed Pin Initialize
+  pinMode(9,OUTPUT);       //Channel B Speed Pin Initialize 
+  pinMode(2,OUTPUT);      //Channel A Direction Pin Initialize
+  pinMode(8,OUTPUT);      //Channel B Direction Pin Initialize
+  pinMode(5,OUTPUT);      //Indicator LED Pin Initialize
+  digitalWrite(5,LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    while (Serial.available() > 0){
+    inByte = Serial.read();
+    if ((inByte >= 65 && inByte <= 90) || (inByte >=97 && inByte <=122) || (inByte >= 48 &&
+    inByte <=57) || inByte == 43 || inByte == 61 || inByte == 63) {
+      command.concat(inByte);
+    }
+  }
+  Mode = command.substring(0,4);
+  Speed1 = command.substring(4,7);
+  Speed2 = command.substring(7);
+  Speed1.toCharArray(Speed1Array,4);
+  Speed2.toCharArray(Speed2Array,4);
+  Speed1Int = atoi(Speed1Array);
+  Speed2Int = atoi(Speed2Array);
+  if (Mode.equalsIgnoreCase("1R2L")){
+      Serial.println("Status is first right, second left");
+      Serial.print("Speed 1 ");
+      Serial.println(Speed1);
+      Serial.print("Speed 2 ");
+      Serial.println(Speed2);
+      digitalWrite(2, HIGH);
+      digitalWrite(8, LOW);
+      analogWrite(3, Speed1Int);
+      analogWrite(9, Speed2Int);
+      delay(500); 
+  }
+  if (Mode.equalsIgnoreCase("1L2R")){
+      Serial.println("Status is first left second right");
+      Serial.print("Speed 1 ");
+      Serial.println(Speed1Int);
+      Serial.print("Speed 2 ");
+      Serial.println(Speed2Int);
+      digitalWrite(2, LOW);
+      digitalWrite(8, HIGH);      
+      analogWrite(3, Speed1Int);
+      analogWrite(9, Speed2Int);
+      delay(500);
+  }
+  if (Mode.equalsIgnoreCase("1R2R")){
+      Serial.println("Status is both right");
+      Serial.print("Speed 1 ");
+      Serial.println(Speed1);
+      Serial.print("Speed 2 ");
+      Serial.println(Speed2);
+      digitalWrite(2, HIGH);
+      digitalWrite(8, HIGH);
+      analogWrite(3, Speed1Int);
+      analogWrite(9, Speed2Int);
+      delay(500);
+  }
+  if (Mode.equalsIgnoreCase("1L2L")){
+      Serial.println("Status is both left");
+      Serial.print("Speed 1 ");
+      Serial.println(Speed1);
+      Serial.print("Speed 2 ");
+      Serial.println(Speed2);
+      digitalWrite(2, LOW);
+      digitalWrite(8, LOW);
+      analogWrite(3, Speed1Int);
+      analogWrite(9, Speed2Int);
+      delay(500);
+  }
+  if (Mode.equalsIgnoreCase("Stan")){
+    Serial.println("Punching, Stand by please");
+    analogWrite(3,0);
+    analogWrite(9,0);
+    digitalWrite(5,HIGH);
+    delay(2500);
+    digitalWrite(5,LOW);
+    Serial.println("Ejecting");
+    digitalWrite(2, LOW);
+    digitalWrite(8, HIGH);
+    analogWrite(3, 128);
+    analogWrite(9, 128);
+    delay(3000);
+    analogWrite(3, 0);
+    analogWrite(9, 0);
+    Serial.println("Ejection complete, Next Letter");
+  }
+  
+  delay(50);
+  command = "";
+}
---
+++ postmaster.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+import serial
+
+#green
+#lowerBound=np.array([33,80,40])
+#upperBound=np.array([102,255,255])
+#red
+lower_bound_red=np.array([0, 100, 100])
+upper_bound_red=np.array([10, 255, 255])
+#white
+sensitivity = 15
+lower_bound_white = np.array([0,0,255-sensitivity])
+upper_bound_white = np.array([255,sensitivity,255])
+
+cam= cv2.VideoCapture(2)
+kernelOpen=np.ones((5,5))
+kernelClose=np.ones((20,20))
+
+ser = serial.Serial('/dev/ttyACM0', 9600)
+#font=cv2.cv.InitFont(cv2.cv.CV_FONT_HERSHEY_SIMPLEX,2,0.5,0,3,1)
+
+def detect_rectangle_by_color(img, lower_bound, upper_bound, img_original):
+    mask=cv2.inRange(img, lower_bound,upper_bound)
+    #morphology
+    #maskOpen=cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernelOpen)
+    #maskClose=cv2.morphologyEx(maskOpen,cv2.MORP"", maskH_CLOSE,kernelClose)
+    cv2.imshow("", mask)
+    #maskFinal=maskClose
+    maskFinal=mask
+    #conts,h=cv2.findContours(maskFinal.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    img, conts, h = cv2.findContours(maskFinal.copy(), cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    
+    cv2.drawContours(img,conts,-1,(255,0,0),3)
+    max_x = 0
+    max_y = 0
+    max_w = 0
+    max_h = 0
+    for i in range(len(conts)):
+        x,y,w,h=cv2.boundingRect(conts[i])
+        if w > max_w and h > max_h:
+            max_x = x
+            max_y = y
+            max_w = w
+            max_h = h
+    if max_w >= 450 and max_w <= 525 and max_h >= 300 and max_h <= 355:
+        cv2.rectangle(img_original, (max_x, max_y), (max_x + max_w, max_y + max_h), (0,0,255), 2)
+        #print("min x: %d" % (max_x))
+        if max_x < 108:
+            ser.write(b'stan000000')
+        return True
+    else:
+        return False
+
+#ser = serial.Serial('/dev/ttyACM0', 9600)
+#`ser.write(b'1l2l100000')
+
+while True:
+    ret, img=cam.read()
+    img=cv2.resize(img,(680,420))
+    img_original = img
+    #img = cv2.medianBlur(img, 3)
+    img = cv2.GaussianBlur(img,(17,17),0)
+    cv2.imshow("blur", img)
+    #convert BGR to HSV
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    if not detect_rectangle_by_color(img_hsv, lower_bound_red, upper_bound_red, img_original):
+        ser.write(b'1r2l128128')
+
+    cv2.imshow("cam",img_original)
+    cv2.waitKey(10)
